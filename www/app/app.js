e8bb0b3badde65f1ae8a6a455800b830bc82653f
Viktor Novak <viktor_novak@epam.com>
2016-04-19 08:38:05 +0300

    first commit

---
+++ www/app/app.js
@@ -0,0 +1,34 @@
+angular.module('eliteApp', ['ionic'])
+
+  .run(function($ionicPlatform) {
+    $ionicPlatform.ready(function() {
+      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+      // for form inputs)
+      if (window.cordova && window.cordova.plugins.Keyboard) {
+        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+        cordova.plugins.Keyboard.disableScroll(true);
+
+      }
+      if (window.StatusBar) {
+        // org.apache.cordova.statusbar required
+        StatusBar.styleDefault();
+      }
+    });
+  })
+
+  .config(function($stateProvider, $urlRouterProvider) {
+    $stateProvider
+
+      .state('home', {
+        url: '/home',
+        templateUrl: 'app/home/home.html'
+      })
+
+      .state('app', {
+        url: '/app',
+        templateUrl: 'app/layout/menu-layout.html'
+      });
+
+    // if none of the above states are matched, use this as the fallback
+    $urlRouterProvider.otherwise('/app');
+  });
